a8fa1a14dbd038798a6d9575b1125b9619411e8b
DongPark987 <55174232+DongPark987@users.noreply.github.com>
2023-12-11 23:10:07 +0900

    가자

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import { RedisIoAdapter } from './redis-io-adapter/redis-io-adapter';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
     app.use(cookieParser());
-
-    //레디스 어뎁터
-    const redisIoAdapter = new RedisIoAdapter(app);
-    await redisIoAdapter.connectToRedis();
-    app.useWebSocketAdapter(redisIoAdapter);
 
     if (!process.env.LISTEN_PORT) {
         throw new Error('LISTEN_PORT is not defined');
--- src/redis-io-adapter/redis-io-adapter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RedisIoAdapter } from './redis-io-adapter';
-
-describe('RedisIoAdapter', () => {
-  it('should be defined', () => {
-    expect(new RedisIoAdapter()).toBeDefined();
-  });
-});
